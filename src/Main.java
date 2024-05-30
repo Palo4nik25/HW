ee4930cbc78f9f71a373e6aaa2920a0aba620138
palo4nik25 <pawlo2005kaplan@gmail.com>
2024-05-30 12:06:17 +0200

    Moved code from another repo

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        float res = 0f;
+//
+//        System.out.print("It's basic multiplication calculator, please enter first number: ");
+//        float num1 = scan.nextFloat();
+//        System.out.print("Please enter second number: ");
+//        float num2 = scan.nextFloat();
+//        System.out.print("Please enter operator: ");
+//        char op = scan.next().charAt(0);
+//
+//        switch (op){
+//            case('+'): res = num1 + num2; break;
+//            case('-'): res = num1 - num2; break;
+//            case('*'): res = num1 * num2; break;
+//            case('/'): res = num1 / num2; break;
+//            default:
+//                System.out.println("Invalid operator");
+//        }
+//
+//        if (res % 2 == 0 || res % 5 == 0) {
+//            System.out.println("Your result is: " + Math.round(res));
+//        } else System.out.println("Your result is: " + res);
+    }
+}
